92ba5d293303c05cd1eb05ae491fa7a53b0e2dd4
extase <76060406+ex7ase@users.noreply.github.com>
2021-05-05 14:05:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 ///////////////////////////////
 ///////////////////////////////
-// STARTUPS ///////////////////
-const { Client, Intents, Collection } = require("discord.js");
-const bot = new Client({
+// VARIABLES //////////////////
+const { Client, Intents, Collection, MessageEmbed } = require("discord.js");
+const bot = new Client();
+
+///////////////////////////////
+///////////////////////////////
+// FUNCTIONS //////////////////
+function embed() {
+	return new MessageEmbed()
+		.setColor("RANDOM");
+};
