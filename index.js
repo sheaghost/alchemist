3da8369e32e510c03e6d8c9275e8bf41f446c2fc
extase <76060406+ex7ase@users.noreply.github.com>
2021-05-05 13:54:29 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+///////////////////////////////
+///////////////////////////////
+// STARTUPS ///////////////////
+const { Client, Intents, Collection } = require("discord.js");
+const bot = new Client({
